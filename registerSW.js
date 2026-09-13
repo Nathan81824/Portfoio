9a3610674aecb074edf038383af7e179f2d3e763
Nathan81824 <Nathanmoses.u@gmail.com>
2026-09-13 01:41:10 -0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Portfoio/sw.js', { scope: '/Portfoio/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
